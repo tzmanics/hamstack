10fb221450bcfcf74628cd3f4ce0d946045ee21c
Tara Z. Manicsic <tzmanics@gmail.com>
2020-03-24 23:52:58 -0400

    adds home page cards

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,15 +1,12 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { MatCardModule } from "@angular/material/card";
 
-import { HomeRoutingModule } from './home-routing.module';
-import { HomeComponent } from './home.component';
-
+import { HomeRoutingModule } from "./home-routing.module";
+import { HomeComponent } from "./home.component";
 
 @NgModule({
   declarations: [HomeComponent],
-  imports: [
-    CommonModule,
-    HomeRoutingModule
-  ]
+  imports: [CommonModule, HomeRoutingModule, MatCardModule]
 })
-export class HomeModule { }
+export class HomeModule {}
